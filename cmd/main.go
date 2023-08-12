ae6da2fa4656fdb776661e18730260fdc5805531
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2023-08-12 17:49:37 +0100

    development

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+
+var ctx = context.Background()
+
 func main() {
+
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+
+	ping, err := client.Ping(ctx).Result()
+	if err != nil {
+		panic(err.Error())
+	}
+
 	
+
+	fmt.Println(ping)
 }
